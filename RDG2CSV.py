24ac28ce95e1566c91a8d0a576d7f2a5565ab5b9
Samuel Odriozola <samuel.martinez2@epicor.com>
2022-12-23 12:37:08 -0600

    Export AHK file

--- RDG2CSV.py
+++ RDG2CSV.py
@@ -31,6 +31,7 @@ def clear_output_folder(filepaths, folder):
                 os.remove(filepath)
                 
 def import_xml(filepath):
+    print(f"Importing {filepath}")
     tree = ET.parse(filepath)
     root = tree.getroot()
     return ET.tostring(root, encoding='utf-8', method='xml')
@@ -63,6 +64,7 @@ def pull_server_properties(server, group, group_name, subgroup_name=""):
     return properties
 
 def standarize_properties(property_list):
+    print(f"Standarizing data")
     std_property_list = []
     for property in property_list:
         std_property = {"displayName":"", "name":"", "group":"", "subgroup":"", "userName":"", "domain":""}
@@ -73,10 +75,23 @@ def standarize_properties(property_list):
     return std_property_list
 
 def export_csv(filepath, headers, dicitonary_list):
+    print(f"Exporting {filepath}")
     with open(filepath, "w") as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames=headers, delimiter=',', quotechar='"', lineterminator="\n", quoting=csv.QUOTE_MINIMAL)
         writer.writeheader()
         writer.writerows(dicitonary_list)
+        
+def export_ahk(filepath, dictionary_list):
+    print(f"Exporting {filepath}")
+    ahk = [
+        "#NoEnv", "; #Warn", "SendMode Input", "SetWorkingDir %A_ScriptDir%", ""
+    ]
+    for entry in dictionary_list:
+        ahk.append(f'::${entry["name"]}::{entry["displayName"]} / {entry["group"]} {entry["subgroup"]} @ {entry["name"]}')
+    with open(filepath, "w") as ahk_file:
+        for element in ahk:
+            ahk_file.write(f"{element}\n")
+    
 
 def main():
     
@@ -85,9 +100,10 @@ def main():
     rdg_filepath_in = validate_file("servers.rdg", folder_in)
     csv_filepath_out = build_filepath("servers.csv", folder_out)
     json_filepath_out = build_filepath("servers.json", folder_out)
+    ahk_filepath_out = build_filepath("servers.ahk", folder_out)
     
     mkdir_output(folder_out)
-    clear_output_folder([csv_filepath_out, json_filepath_out], folder_out)
+    clear_output_folder([csv_filepath_out, json_filepath_out, ahk_filepath_out], folder_out)
     
     rdg_xml = import_xml(rdg_filepath_in)
     rdg_dict = xmltodict.parse(rdg_xml)
@@ -99,6 +115,7 @@ def main():
     output_list = []
     
     # This loop will only traverse for a single sub-level (sub-group)
+    print(f"Processing data")
     for group in rdg_group_list:
         group_name = group["properties"]["name"]
         if "server" in group.keys():
@@ -113,6 +130,8 @@ def main():
                                 
     output_list = standarize_properties(output_list)
     export_csv(csv_filepath_out, output_header, output_list)
+    export_ahk(ahk_filepath_out, output_list)
+    print(f"Finished")
         
 if __name__ == "__main__":
     main()
